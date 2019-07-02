bacc99ebf5902fa7ad55061e7a62fbb6a43b3228
Adam Trzciński <trzcinski.adam@gmail.com>
2019-07-02 21:16:33 +0200

    Initial Commit

---
+++ Todooz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todooz
+//
+//  Created by adam on 02/07/2019.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
